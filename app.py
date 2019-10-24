d0f120760d196617b55012fb7373c5ae2fa05b26
adiXcodr <ichbinadittyadey@gmail.com>
2019-10-25 02:22:01 +0530

    Audio input

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, flash, redirect, url_for
 from textblob import TextBlob as tb
+import speech_recognition as sr
+import os
 
 app = Flask(__name__)
 
@@ -8,10 +10,23 @@ def home():
     return render_template('app.html')
 
 @app.route('/predict',methods=['POST'])
+
 def predict():
     a= request.form.get('movietext')
+    if(not a):
+        try:
+            r = sr.Recognizer()
+            f = request.files['file']  
+            f.save(f.filename)
+            sampleaudio = sr.AudioFile(f.filename)
+            with sampleaudio as source:
+                audio = r.record(source)
+            a=r.recognize_google(audio)
+            os.remove(f.filename)
+        except KeyError:   
+            return render_template('app.html', prediction_text='ERROR: Enter some text or audio') 
     blob=tb(a)
-    rate=5.333333333333333+blob.sentiment.polarity*4.5
+    rate=5+blob.sentiment.polarity*5
     rate=round(rate,1)
     output=rate
     return render_template('app.html', prediction_text='Rating: {}/10'.format(output))
@@ -20,7 +35,7 @@ def predict():
 def predict_api():
     a= data.get('movietext')
     blob=tb(a)
-    rate=5.333333333333333+blob.sentiment.polarity*4.5
+    rate=5+blob.sentiment.polarity*5
     rate=round(rate,1)
     output=rate
     return jsonify(output)
