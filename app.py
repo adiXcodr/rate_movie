154dc8f00ae664bb44aaa8e6f4ee0ecb1558f31c
adiXcodr <ichbinadittyadey@gmail.com>
2019-10-27 09:21:25 +0530

    Audio input

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template, flash, redirect, url_for
 from textblob import TextBlob as tb
-import speech_recognition as sr
-import os
+#import speech_recognition as sr
+#import os
 
 app = Flask(__name__)
 
@@ -12,19 +12,19 @@ def home():
 @app.route('/predict',methods=['POST'])
 
 def predict():
-    a= request.form.get('movietext')
-    if(not a):
-        try:
-            r = sr.Recognizer()
-            f = request.files['file']  
-            f.save(f.filename)
-            sampleaudio = sr.AudioFile(f.filename)
-            with sampleaudio as source:
-                audio = r.record(source)
-            a=r.recognize_google(audio)
-            os.remove(f.filename)
-        except KeyError:   
-            return render_template('app.html', prediction_text='ERROR: Enter some text or audio') 
+    a= request.form.get('eng-input')
+    #if(not a):
+      #  try:
+        #    r = sr.Recognizer()
+        #    f = request.files['file']  
+         #   f.save(f.filename)
+          #  sampleaudio = sr.AudioFile(f.filename)
+           # with sampleaudio as source:
+            #    audio = r.record(source)
+           # a=r.recognize_google(audio)
+           # os.remove(f.filename)
+       # except KeyError:   
+           # return render_template('app.html', prediction_text='ERROR: Enter some text or audio') 
     blob=tb(a)
     rate=5+blob.sentiment.polarity*5
     rate=round(rate,1)
@@ -33,7 +33,7 @@ def predict():
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
-    a= data.get('movietext')
+    a= data.get('eng-input')
     blob=tb(a)
     rate=5+blob.sentiment.polarity*5
     rate=round(rate,1)
