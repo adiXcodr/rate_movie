bc872e286bdb603211c015bf1ff9e925c59ba11c
adiXcodr <ichbinadittyadey@gmail.com>
2020-01-18 12:03:32 +0530

    keepawake

--- app.py
+++ app.py
@@ -13,18 +13,6 @@ def home():
 
 def predict():
     a= request.form.get('eng-input')
-    #if(not a):
-      #  try:
-        #    r = sr.Recognizer()
-        #    f = request.files['file']  
-         #   f.save(f.filename)
-          #  sampleaudio = sr.AudioFile(f.filename)
-           # with sampleaudio as source:
-            #    audio = r.record(source)
-           # a=r.recognize_google(audio)
-           # os.remove(f.filename)
-       # except KeyError:   
-           # return render_template('app.html', prediction_text='ERROR: Enter some text or audio') 
     blob=tb(a)
     rate=5+blob.sentiment.polarity*5
     rate=round(rate,1)
